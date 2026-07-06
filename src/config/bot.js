ef45a31a9d84275246e34818f78a1f9dd3df2ecd
pakid9800857844-sys <pakid9800857844@gmail.com>
2026-07-06 20:40:24 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -107,7 +107,7 @@ export const botConfig = {
 
       // Discord-style palette shortcuts.
       blurple: "#5865F2",
-      green: "#57F287",
+      green: "#800080",
       yellow: "#FEE75C",
       fuchsia: "#EB459E",
       red: "#ED4245",
